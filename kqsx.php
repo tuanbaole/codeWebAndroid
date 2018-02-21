bdd82a519cf6dc994964b92f5e54449aef8d1ce4
baotuanle <letuanbao1993@gmail.com>
2018-02-21 09:07:20 +0700

    kqsx-boso

--- kqsx.php
+++ kqsx.php
@@ -1,10 +1,87 @@
-<?php
+﻿<?php
 date_default_timezone_set("Asia/Ho_Chi_Minh");
 if(isset($_GET["date"])){
 	$date = $_GET["date"];
 } else {
 	$date = date("d-m-Y");
 }
+$myfile = fopen("FileLog.txt", "a+") or die("Unable to open file!");
+$txt = date("d-m-Y H : i : s A")." >>>> ".$_SERVER['REMOTE_ADDR']." : ".$_SERVER['HTTP_USER_AGENT']."\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+if ($_SERVER['HTTP_USER_AGENT'] == 'Dalvik/2.1.0 (Linux; U; Android 7.0; SM-J730G Build/NRD90M)') {
+	// echo '{"link":[""],"ketqua":[false]}';
+	// die();
+}
+// $ketqua["link"][] = $date;
+// $ketqua["ketqua"][] = "69463";
+// $ketqua["ketqua"][] = "63885";
+// $ketqua["ketqua"][] = "58043 - 68562";
+// $ketqua["ketqua"][] = "08290 - 48249 - 90725 - 61815 - 73354 - 79862";
+// $ketqua["ketqua"][] = "5708 - 2001 - 7276 - 7111";
+// $ketqua["ketqua"][] = "4202 - 5585 - 4054 - 6736 - 7770 - 7922";
+// $ketqua["ketqua"][] = "896 - 943 - 594";
+// $ketqua["ketqua"][] = "52 - 80 - 48 - 71";
+// echo json_encode($ketqua);
+// die();
+$page = 1;
+if($page == 1) {
+$urlMe = 'http://xoso.me/kqxs-ket-qua-xo-so-ngay-'.$date.'.html';
+$arrContextOptionsMe=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+$responseMe = file_get_contents($urlMe, false, stream_context_create($arrContextOptionsMe));
+$first_step_me = explode( "id=\"load_kq_mb_0\">" , $responseMe );
+if (count($first_step_me) == 2) {
+	$checkgetkq = "";
+	$second_step_me = explode( "</table>" , $first_step_me[1] );
+	$findMe1 = array("Đặc biệt","Giải nhất","Giải nhì","Giải ba","Giải tư","Giải năm","Giải sáu","Giải bảy");
+	$replaceMe2 = array("kq","kq","kq","kq","kq","kq","kq","kq");
+	$arr_three_step_me = str_replace($findMe1,$replaceMe2,trim(strip_tags($second_step_me[0])));
+	$showValMe = explode("kq" , $arr_three_step_me);
+	$ketqua["link"][] = $date;
+	foreach ($showValMe as $keyShowValMe => $str) {
+		switch ($keyShowValMe) {
+				case 1:
+					$checkgetkq = str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str)));
+					$ketqua["ketqua"][] = str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str)));
+					break;
+				case 2:
+					$ketqua["ketqua"][] = str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str)));
+					break;
+				case 3:
+					$ketqua["ketqua"][] = substr(chunk_split(preg_replace('/\s\s+/', ' ', trim($str)), 5, ' - '),0,-3);
+					break;
+				case 4:
+					$ketqua["ketqua"][] =  substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 5, ' - '),0,-3);
+					break;
+				case 5:
+					$ketqua["ketqua"][] =  substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 4, ' - '),0,-3);
+					break;
+				case 6:
+					$ketqua["ketqua"][] =  substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 4, ' - '),0,-3);
+					break;
+				case 7:
+					$ketqua["ketqua"][] =  substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 3, ' - '),0,-3);
+				break;
+				case 8:
+					//$checkgetkq = substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 2, ' - '),0,-3);
+					$ketqua["ketqua"][] =  substr(chunk_split(str_replace(" ","",trim(preg_replace('/\s\s+/', ' ', $str))), 2, ' - '),0,-3);
+				break;
+			}
+	}
+	if ($checkgetkq == "") {
+		echo '{"link":[""],"ketqua":[false]}';
+	} else {
+		echo json_encode($ketqua);
+	}
+	
+}
+die();
+} else {
 $url = 'http://ketqua.net/xo-so-truyen-thong.php?ngay='.$date;
 $arrContextOptions=array(
     "ssl"=>array(
@@ -66,13 +143,6 @@ if (count($first_step) == 2) {
 	$ketqua["link"][] = "";
 	$ketqua["ketqua"][] = false;
 }
-// $ketqua["link"][] = $date;
-// $ketqua["ketqua"][] = "51627";
-// $ketqua["ketqua"][] = "81694";
-// $ketqua["ketqua"][] = "22406 - 03325";
-// $ketqua["ketqua"][] = "97493 - 52600 - 89343 - 83889 - 11197 - 17036";
-// $ketqua["ketqua"][] = "4689 - 0288 - 5341 - 9943";
-// $ketqua["ketqua"][] = "2417 - 1941 - 3604 - 9925 - 4853 - 8079";
-// $ketqua["ketqua"][] = "464 - 043 - 007";
-// $ketqua["ketqua"][] = "90 - 92 - 38 - 01";
 echo json_encode($ketqua);
+die();
+}
